9300d29f65e82e99eb2d261e9432e7984f527357
YvonneJL <yvonneloder@gmx.de>
2025-02-26 19:20:23 +0100

    added counter

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -8,6 +8,11 @@ const Game = () => {
   const [cpu, setCpu] = useState("");
   const [result, setResult] = useState("");
 
+  //States für Spielstand und Runden
+  const [roundsWonByPlayer, setRoundWonByPlayer] = useState(0)
+  const [roundsWonByCPU, setRoundWonByCPU] = useState(0)
+  const [round, setRound] = useState(0)
+
   //asynchrone Funktion
   //Wahl cpu in Zwischenergebnis speichern
   //Die Auswahl vom Player durch Parameter gesetzt (eine Auswahl aus dem Array)
@@ -22,14 +27,19 @@ const Game = () => {
       (selection === "Rock" && cpuChoice === "Scissors")
     ) {
       setResult("You Win");
+      setRoundWonByPlayer(roundsWonByPlayer + 1)
+      setRound(round +1)
     } else if (
       (selection === "Scissors" && cpuChoice === "Rock") ||
       (selection === "Paper" && cpuChoice === "Scissors") ||
       (selection === "Rock" && cpuChoice === "Paper")
     ) {
       setResult("You Lose");
+      setRoundWonByCPU(roundsWonByCPU + 1)
+      setRound(round +1)
     } else if (selection === cpuChoice) {
       setResult("Draw");
+      setRound(round +1)
     }
   };
 
@@ -38,35 +48,38 @@ const Game = () => {
     setPlayer("");
     setCpu("");
     setResult("");
+    setRoundWonByCPU(0);
+    setRoundWonByPlayer(0);
+    setRound(0);
   };
 
   return (
     <>
       <section className="flex flex-col items-center mb-30 lg:mb-10">
-        <h1 className="text-2xl lg:text-6xl my-5">
+        <h1 className="text-2xl lg:text-6xl my-2">
           {allOptions[0]} {allOptions[1]} {allOptions[2]}
         </h1>
-        <h3 className="lg:text-3xl">Click to choose</h3>
+        <h3 className="lg:text-2xl">Click to choose</h3>
       </section>
       <section>
-        <div className="flex gap-10 mb-20 lg:mb-10">
+        <div className="flex gap-10 mb-15 lg:mb-10">
           <figure
             onClick={() => handleClick(allOptions[0])}
-            className="cursor-pointer w-20 lg:w-50 flex flex-col items-center lg:gap-3"
+            className="cursor-pointer w-20 lg:w-35 flex flex-col items-center lg:gap-3 transition-transform duration-500 ease-in-out transform hover:scale-125"
           >
             <img src="/pngaaa.com-3313783.png" alt="Rock" />
             <figcaption className="lg:text-2xl">{allOptions[0]}</figcaption>
           </figure>
           <figure
             onClick={() => handleClick(allOptions[1])}
-            className="cursor-pointer w-20 lg:w-50 flex flex-col items-center lg:gap-3"
+            className="cursor-pointer w-20 lg:w-35 flex flex-col items-center lg:gap-3 transition-transform duration-500 ease-in-out transform hover:scale-125"
           >
             <img src="/pngaaa.com-3313779.png" alt="Paper" />
             <figcaption className="lg:text-2xl">{allOptions[1]}</figcaption>
           </figure>
           <figure
             onClick={() => handleClick(allOptions[2])}
-            className="cursor-pointer w-20 lg:w-50 flex flex-col items-center lg:gap-3"
+            className="cursor-pointer w-20 lg:w-35 flex flex-col items-center lg:gap-3 transition-transform duration-500 ease-in-out transform hover:scale-125"
           >
             <img src="/pngaaa.com-3313815.png" alt="Scissors" />
             <figcaption className="lg:text-2xl">{allOptions[2]}</figcaption>
@@ -74,17 +87,20 @@ const Game = () => {
         </div>
       </section>
       <section className="flex flex-col gap-3 mb-5 items-center">
-        <div className="flex items-center gap-2">
-        <p>Player:</p>
+      <h2 className={`${round === 0 ? "invisible": ""} text-center text-2xl lg:text-4xl font-bold mb-7 lg:mb-2`}>Round {round}</h2>
+        <div className="flex items-center w-30 justify-between">
         {player === allOptions[0] && <img className="w-7 h-7" src="/pngaaa.com-3313783.png" alt="Rock" />}
         {player === allOptions[1] && <img className="w-7 h-7" src="/pngaaa.com-3313779.png" alt="Paper" />}
         {player === allOptions[2] && <img className="w-7 h-7" src="/pngaaa.com-3313815.png" alt="Paper" />}
+        <p>YOU:</p>
+        <p className={`${round === 0 ? "invisible": ""} font-bold`}>{roundsWonByPlayer}</p>
         </div>
-       <div className="flex items-center gap-5">
-       <p>CPU:</p>
+       <div className="flex items-center justify-between w-30">
        {cpu === allOptions[0] && <img className="w-7 h-7" src="/pngaaa.com-3313783.png" alt="Rock" />}
        {cpu === allOptions[1] && <img className="w-7 h-7" src="/pngaaa.com-3313779.png" alt="Paper" />}
        {cpu === allOptions[2] && <img className="w-7 h-7" src="/pngaaa.com-3313815.png" alt="Paper" />}
+       <p>CPU:</p>
+       <p className={`${round === 0 ? "invisible": ""} font-bold`}>{roundsWonByCPU}</p>
        </div>
         <p className="font-bold text-2xl mt-3">{result}</p>
       </section>
